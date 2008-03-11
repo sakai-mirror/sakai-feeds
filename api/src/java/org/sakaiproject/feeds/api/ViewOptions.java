6bd407175a4189de79c83dc9993fb82fbc82ec3c
nuno@ufp.pt <nuno@ufp.pt@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2008-03-11 11:43:47 +0000

    NOJIRA: Changed default view for: 'Titles' from 'Last 7 days'
    git-svn-id: https://source.sakaiproject.org/contrib/ufp/newsfeeds/trunk@46557 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- api/src/java/org/sakaiproject/feeds/api/ViewOptions.java
+++ api/src/java/org/sakaiproject/feeds/api/ViewOptions.java
@@ -11,12 +11,12 @@ public interface ViewOptions extends Serializable {
 	public static final String	VIEW_FILTER_TODAY		= "view.today";
 	public static final String	VIEW_FILTER_LAST_WEEK	= "view.last.week";
 	public static final String	VIEW_FILTER_LAST_MONTH	= "view.last.month";
-	public static final String	DEFAULT_VIEW_FILTER		= VIEW_FILTER_TODAY;
+	public static final String	DEFAULT_VIEW_FILTER		= VIEW_FILTER_LAST_WEEK;
 	
 	public static final String	VIEW_DETAIL_FULL_ENTRY	= "view.entry.full";
 	public static final String	VIEW_DETAIL_TITLE_ENTRY	= "view.entry.title";
 	public static final String	VIEW_DETAIL_NO_ENTRY	= "view.entry.none";
-	public static final String	DEFAULT_VIEW_DETAIL		= VIEW_DETAIL_FULL_ENTRY;
+	public static final String	DEFAULT_VIEW_DETAIL		= VIEW_DETAIL_TITLE_ENTRY;
 
 	public String getViewFilter();
 	public void setViewFilter(String viewFilter);
